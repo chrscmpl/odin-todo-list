774d86ee577d41dc39c61409b138ff9efa3bb53b
chrscmpl <chcampolongo1@gmail.com>
2022-09-08 15:55:03 +0200

    Very basic style

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,5 +3,7 @@ import './footer.css';
 export default function Footer() {
 	const footer = document.createElement('div');
 	footer.className = 'footer';
+	footer.innerHTML =
+		'<a class="link" href="https://github.com/chrscmpl/odin-todo-list">copyright chrscmpl</a>';
 	return footer;
 }
