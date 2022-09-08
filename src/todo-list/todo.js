a3f671134ad93ccec06857d6ff68fde90e0934bd
chrscmpl <chcampolongo1@gmail.com>
2022-09-08 15:39:03 +0200

    enum for todo's priority levels

--- src/todo-list/todo.js
+++ src/todo-list/todo.js
@@ -3,11 +3,13 @@ export default class Todo {
 		title,
 		description = '',
 		dueDate = '',
-		priority = '',
+		priority = 'normal',
 		notes = '',
 		checked = false,
 	} = {}) {
 		if (!title) throw new Error('cannot instantiate todo with no title');
+		if (!this.#validPriority(priority))
+			throw new Error('Invalid priority level');
 		Object.assign(this, {
 			title,
 			description,
@@ -17,4 +19,17 @@ export default class Todo {
 			checked,
 		});
 	}
+
+	#validPriority(priority) {
+		const levels = Object.keys(this.priorityLevels);
+		for (let i = 0; i < levels.length; i++)
+			if (priority === this.priorityLevels[levels[i]]) return true;
+		return false;
+	}
 }
+
+Todo.prototype.priorityLevels = {
+	low: 'low',
+	normal: 'normal',
+	high: 'high',
+};
